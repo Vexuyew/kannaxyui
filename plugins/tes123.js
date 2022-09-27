49249467808a8a0d0ef3d80e7b80c23149113c6f
Vexuyew <109408800+Vexuyew@users.noreply.github.com>
2022-09-27 16:11:03 +0700

    Update tes123.js

--- plugins/tes123.js
+++ plugins/tes123.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import fetch from 'node-fetch'
 let handler  = async (m, { conn, usedPrefix: _p }) => {
-let info = `Kanna Disini (>ω<)`
+let info = `Paan tas tes tas tes (>ω<)`
 
 let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
 /*const message = {
